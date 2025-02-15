c6d02aa98b865f0737e4c99aa753c1d30b4b28a3
naymHdev <naym100m@gmail.com>
2025-02-15 10:55:32 +0600

    [created] register form use shadcn form and react use form

---
+++ src/app/register/page.tsx
@@ -0,0 +1,13 @@
+import RegisterForm from "@/components/modules/auth/register/RegisterForm";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <div className=" h-screen w-screen flex items-center justify-center">
+        <RegisterForm />
+      </div>
+    </>
+  );
+};
+
+export default RegisterPage;
