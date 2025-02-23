02fda672c0ec22c7168466d26a5e14dc09660f75
naymHdev <naym100m@gmail.com>
2025-02-23 10:04:05 +0600

    [initial setup] next.js with redux & toolkit

--- src/app/(WithCommonLayout)/cart/page.tsx
+++ src/app/(WithCommonLayout)/cart/page.tsx
@@ -1,5 +1,6 @@
 import CartProducts from "@/components/modules/cart/CartProducts"
 import Coupon from "@/components/modules/cart/Coupon"
+import PaymentDetails from "@/components/modules/cart/PaymentDetails"
 import ProductBanner from "@/components/modules/products/banner"
 import NMContainer from "@/components/ui/core/NMContainer"
 
@@ -12,6 +13,7 @@ const CartPage = () => {
                 <section className="grid grid-cols-12 gap-8 my-5">
                     <CartProducts />
                     <Coupon />
+                    <PaymentDetails />
                 </section>
             </NMContainer>
         </>
