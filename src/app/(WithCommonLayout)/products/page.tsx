d7a9fb210b22036f47b0afe009cee577fde981e8
naymHdev <naym100m@gmail.com>
2025-02-27 10:33:40 +0600

    [created] advance filtaring in product page

--- src/app/(WithCommonLayout)/products/page.tsx
+++ src/app/(WithCommonLayout)/products/page.tsx
@@ -1,30 +1,38 @@
-import AllProducts from '@/components/modules/products';
-import ProductBanner from '@/components/modules/products/banner'
-import CategoryCard from '@/components/ui/core/CategoryCard';
-import NMContainer from '@/components/ui/core/NMContainer'
-import { getAllCategories } from '@/services/Category';
-import { getAllProducts } from '@/services/product';
-import { ICategory } from '@/types/category.types';
-import React from 'react'
+import AllProducts from "@/components/modules/products";
+import ProductBanner from "@/components/modules/products/banner";
+import CategoryCard from "@/components/ui/core/CategoryCard";
+import NMContainer from "@/components/ui/core/NMContainer";
+import { getAllCategories } from "@/services/Category";
+import { getAllProducts } from "@/services/product";
+import { ICategory } from "@/types/category.types";
+import React from "react";
 
-const AllProductsPage = async () => {
-    const { data: categories } = await getAllCategories();
-    const { data: products } = await getAllProducts();
-    return (
-        <>
-            <NMContainer>
-                <ProductBanner title='All Products' path='Home - Products' />
-                <h2 className="text-xl font-bold my-5">Featured Collection </h2>
-                <div className="grid grid-cols-6 gap-6">
-                    {categories?.map((category: ICategory, idx: number) => (
-                        <CategoryCard key={idx} category={category} />
-                    ))}
-                </div>
+type TSearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
-                <AllProducts products={products} />
-            </NMContainer>
-        </>
-    )
-}
+const AllProductsPage = async ({
+  searchParams,
+}: {
+  searchParams: TSearchParams;
+}) => {
+  const query = await searchParams;
+  const { data: categories } = await getAllCategories();
+  const { data: products } = await getAllProducts(undefined, undefined, query);
 
-export default AllProductsPage
+  return (
+    <>
+      <NMContainer>
+        <ProductBanner title="All Products" path="Home - Products" />
+        <h2 className="text-xl font-bold my-5">Featured Collection </h2>
+        <div className="grid grid-cols-6 gap-6">
+          {categories?.map((category: ICategory, idx: number) => (
+            <CategoryCard key={idx} category={category} />
+          ))}
+        </div>
+
+        <AllProducts products={products} />
+      </NMContainer>
+    </>
+  );
+};
+
+export default AllProductsPage;
--- src/components/modules/products/index.tsx
+++ src/components/modules/products/index.tsx
@@ -1,26 +1,24 @@
-import ProductCard from "@/components/ui/core/ProductCard"
-import { IProduct } from "@/types/product.type"
-import FilterSidebar from "./filterSidebar"
-
+import ProductCard from "@/components/ui/core/ProductCard";
+import { IProduct } from "@/types/product.type";
+import FilterSidebar from "./filterSidebar";
 
 const AllProducts = ({ products }: { products: IProduct[] }) => {
-    return (
-        <>
-            <div className=" flex gap-8 my-10">
-                <div>
-                    <FilterSidebar />
-                </div>
-                <div>
-                    <div className="grid grid-cols-3 gap-8">
-                        {products?.map((product: IProduct, idx: number) => (
-                            <ProductCard key={idx} product={product} />
-                        ))}
-                    </div>
+  return (
+    <>
+      <div className=" flex gap-8 my-10">
+        <div className=" w-full max-w-[20rem]">
+          <FilterSidebar />
+        </div>
+        <div>
+          <div className="grid grid-cols-3 gap-8">
+            {products?.map((product: IProduct, idx: number) => (
+              <ProductCard key={idx} product={product} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default AllProducts
+export default AllProducts;
